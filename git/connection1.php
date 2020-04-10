a6bf37a101ae2c324797843bf5082494f07300dd
Thanh Mui <cogaiit2000@gmail.com>
2020-04-10 16:28:40 +0700

    Bai2Tuan3

---
+++ git/connection1.php
@@ -0,0 +1,6 @@
+ <?php $servername="localhost";//địa chỉ IP máy chủ
+	 $username="root";//tên đăng nhập
+	 $password="";//mật khẩu truy cập
+ $dbname="blogs";
+     $corn=new mysqli($servername,$username,$password,$dbname);//tạo kết nối cơ sở dữ liệu-  -->
+     ?>
